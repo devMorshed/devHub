7b722748d0edd0463bab095de8e5fa474fc20ce9
devMorshed <ac3m0r@gmail.com>
2023-04-10 02:30:13 +0600

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import Jobdetails from './pages/Jobdetails'
+import Statistics from './pages/Statistics'
+import Blogs from './pages/Blogs'
+import AppliedJobs from './pages/AppliedJobs'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>,
+      },
+      {
+        path: '/applied',
+        element: <AppliedJobs/>,
+      },
+      {
+        path: '/jobdetails',
+        element: <Jobdetails/>,
+      },
+      {
+        path: '/statistics',
+        element: <Statistics/>,
+      },
+      {
+        path: '/blogs',
+        element: <Blogs/>,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={ router} />
 )
---
+++ src/pages/Jobdetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Jobdetails = () => {
+  return (
+    <div>
+      <h1>This is selected job details</h1>
+    </div>
+  );
+};
+
+export default Jobdetails;
