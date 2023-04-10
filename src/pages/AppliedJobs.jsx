ff3ef7aaf38e534f9f2acfcb79a8f0361c27c8b1
devMorshed <ac3m0r@gmail.com>
2023-04-10 20:19:18 +0600

    getting data from local storage to applied jobs

--- src/pages/AppliedJobs.jsx
+++ src/pages/AppliedJobs.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getAppliedJobs } from "../utils/utils";
 
 const AppliedJobs = () => {
-  return (
-    <div>
-      <h2>These are all  applied jobs </h2>
-    </div>
-  );
+	const appliedJobs = getAppliedJobs();
+	// console.log(appliedJobs);
+
+	const [applied, setApplied] = useState([]);
+	useEffect(() => {
+		fetch("jobs.json")
+			.then((res) => res.json())
+      .then((data) => {
+        
+				let arr = [];
+				for (const key in appliedJobs) {
+					const jobb = data.jobs.find((j) => j.id === parseInt(key));
+          arr.push(jobb)
+				}
+        setApplied(arr);
+			});
+	}, []);
+
+	console.log(applied);
+	return (
+		<div className="my-10">
+      <p className="text-4xl text-center ">Applied In { applied.length} jobs </p>
+		</div>
+	);
 };
 
 export default AppliedJobs;
